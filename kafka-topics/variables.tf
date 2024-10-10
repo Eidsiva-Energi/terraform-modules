df875767a0e03f77a7fddbff58458570803b0359
Thomas Bakken Moe <thom@computas.com>
2024-10-10 15:33:32 +0200

    Removed duplicate variables

--- kafka-topics/variables.tf
+++ kafka-topics/variables.tf
@@ -117,47 +117,6 @@ variable "connectors" {
 variable "environment_id" {}
 variable "cluster_id" {}
 
-variable "connectors" {
-  type = map(object({
-    name           = string
-    system_name    = string
-    is_sink        = bool
-    format         = string
-    account_name   = string
-    account_key    = string
-    container_name = string
-  }))
-  default = {}
-}
-
-variable "environment_id" {}
-variable "cluster_id" {}
-
-
-###############################
-# Schema
-###############################
-
-#variable "schema" {
-#  type        = string
-#  default     = ""
-#  description = "The schema to upload to the Schema Registry."
-#  validation {
-#    condition     = var.schema != "" ? contains([".avsc", ".json"], substr(var.schema, -5, -1)) : true
-#    error_message = "The schema must be an avro or json schema, and the value must end with .avsc or .json."
-#  }
-#}
-
-variable "schema" {
-  type        = string
-  default     = ""
-  description = "The path to the schema to upload to the Schema Registry."
-  validation {
-    condition     = var.schema != "" ? contains([".json"], substr(var.schema, -5, -1)) : true
-    error_message = "The schema must be an json schema, and the value must end with .json."
-  }
-}
-
 
 ###############################
 # Schema
