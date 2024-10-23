74b49272b556dff9e821e8cabd795677914757a6
Oscar Bergan <ober@computas.com>
2024-10-23 15:01:04 +0200

    Adds module for azurerm rg

---
+++ azurerm/resource-group/variables.tf
@@ -0,0 +1,12 @@
+variable "environment" {
+  description = "The environment of the resource group."
+  type        = string
+}
+variable "name" {
+  description = "The name of the resource group."
+  type        = string
+}
+variable "location" {
+  description = "The location of the resource group."
+  type        = string
+}
