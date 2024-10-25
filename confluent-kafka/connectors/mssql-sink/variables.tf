d7e1352ac92b9e6717111afacc4c158acedce915
Oscar Bergan <ober@computas.com>
2024-10-25 08:59:01 +0200

    Update format of var pk.mode for MsSql sink module

--- confluent-kafka/connectors/mssql-sink/variables.tf
+++ confluent-kafka/connectors/mssql-sink/variables.tf
@@ -106,7 +106,7 @@ variable "pk_mode" {
 }
 
 variable "pk_fields" {
-  description = "List of comma-separated primary key field names. Its interpretation depends on pk.mode."
-  type        = list(string)
-  default     = ["__connect_topic", "__connect_partition", "__connect_offset"]
+  description = "Comma-separated string of primary key field names. Its interpretation depends on pk.mode."
+  type        = string
+  default     = "__connect_topic,__connect_partition,__connect_offset"
 }
