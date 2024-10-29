d3abfb4c44b17de967949099d1d96377a9f7b813
Thomas Bakken Moe <thom@computas.com>
2024-10-29 10:36:29 +0100

    Remove testing validation

--- confluent-kafka/topic/variables.tf
+++ confluent-kafka/topic/variables.tf
@@ -156,10 +156,6 @@ variable "schema_path" {
     condition     = can(regex(".*\\.(json|avro)$", var.schema_path))
     error_message = "The schema_path must point to a .json or .avro file."
   }
-  validation {
-    condition     = length(var.schema_format) > 0
-    error_message = "TEST"
-  }
 }
 
 variable "schema_format" {
