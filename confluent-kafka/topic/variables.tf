9f8734b58420bb274187f435b9a58b6498ea3594
Thomas Bakken Moe <thom@computas.com>
2024-10-29 13:37:21 +0100

    Change validation to not use local.schema_file

--- confluent-kafka/topic/variables.tf
+++ confluent-kafka/topic/variables.tf
@@ -165,7 +165,7 @@ variable "schema_path" {
     error_message = "The schema file must be a valid JSON file."
   }
   validation {
-    condition     = var.schema_format == "AVRO" ? schema_file.type == "record" : true
+    condition     = var.schema_format == "AVRO" ? jsondecode(file(var.schema_path)).type == "record" : true
     error_message = "The schema file must be a valid AVRO file with type = 'Record'"
   }
 }
