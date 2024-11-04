297ac16612aedffdf6b3537c9ea6a4ecf13a2caa
Thomas Bakken Moe <thom@computas.com>
2024-11-04 16:09:00 +0100

    Corrects variable reference

--- confluent-kafka/topic/main.tf
+++ confluent-kafka/topic/main.tf
@@ -90,7 +90,7 @@ locals {
   schemaJson = jsondecode(local.schema)
 }
 resource "confluent_schema" "schema" {
-  count = use_producer_defined_schema ? 0 : 1
+  count = var.schema_configuration.use_producer_defined_schema ? 0 : 1
 
   depends_on   = [confluent_kafka_topic.topic]
   subject_name = "${confluent_kafka_topic.topic.topic_name}-value"
