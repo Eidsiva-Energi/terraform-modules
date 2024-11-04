714ec6119a3d6ab31f990ed75e8b591ff3974366
Thomas Bakken Moe <thom@computas.com>
2024-11-04 08:39:58 +0100

    Corrects validation logic

--- confluent-kafka/topic/variables.tf
+++ confluent-kafka/topic/variables.tf
@@ -157,7 +157,7 @@ variable "schema_configuration" {
     condition = (
       (var.schema_configuration.schema_path != null && var.schema_configuration.schema_format != null && var.schema_configuration.use_producer_defined_schema == false)
       ||
-      (var.schema_configuration.schema_path != null && var.schema_configuration.schema_format != null && var.schema_configuration.use_producer_defined_schema == true)
+      (var.schema_configuration.schema_path == null && var.schema_configuration.schema_format == null && var.schema_configuration.use_producer_defined_schema == true)
     )
     error_message = "Either schema_path and schema_format must be set, or use_producer_defined_schema must be true."
   }
