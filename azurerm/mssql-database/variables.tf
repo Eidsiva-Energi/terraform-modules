fb100d5cddf60cbf35fdbbbb1a4f7314aa6ecbab
Thomas Bakken Moe <thom@computas.com>
2024-12-13 08:39:09 +0100

    Test using azure-locations file in parent folder

--- azurerm/mssql-database/variables.tf
+++ azurerm/mssql-database/variables.tf
@@ -26,7 +26,7 @@ variable "location_override" {
   default     = ""
 
   validation {
-    condition     = var.location_override == "" || can(regex("^(${replace(replace(file("${path.module}/azure-locations.txt"), "\r\n", "|"), "\n", "|")})$", var.location_override))
+    condition     = var.location_override == "" || can(regex("^(${replace(replace(file("${path.module}/../azure-locations.txt"), "\r\n", "|"), "\n", "|")})$", var.location_override))
     error_message = "Invalid Azure location. Value must be one of the following: [${replace(replace(file("${path.module}/azure-locations.txt"), "\r\n", ", "), "\n", ", ")}]"
   }
 }
