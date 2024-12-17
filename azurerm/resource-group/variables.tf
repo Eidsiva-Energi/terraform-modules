c8f2c71cecba129365fb4e6135e4ed832fc82fbf
Thomas Bakken Moe <thom@computas.com>
2024-12-17 12:59:29 +0100

    Adds validation to location

--- azurerm/resource-group/variables.tf
+++ azurerm/resource-group/variables.tf
@@ -9,4 +9,9 @@ variable "name" {
 variable "location" {
   description = "The location of the resource group."
   type        = string
+
+  validation {
+    condition     = var.location == "" || can(regex("^(${replace(replace(file("${path.module}/../allowed-azure-locations.txt"), "\r\n", "|"), "\n", "|")})$", var.location))
+    error_message = "Invalid Azure location. Value must be one of the following: [${replace(replace(file("${path.module}/../allowed-azure-locations.txt"), "\r\n", ", "), "\n", ", ")}]"
+  }
 }
