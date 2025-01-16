c349afe4e07ba2156ef05dbed66596c9c5ca40fd
Thomas Bakken Moe <thom@computas.com>
2025-01-16 13:26:58 +0100

    Fixed naming mistake

--- azurerm/storage-account/variables.tf
+++ azurerm/storage-account/variables.tf
@@ -12,7 +12,7 @@ variable "name_override" {
   type    = string
 
   validation {
-    condition     = can(regex("^[a-z0-9]{3,24}$", var.name))
+    condition     = can(regex("^[a-z0-9]{3,24}$", var.name_override))
     error_message = "${var.name} is an invalid name. The name must be between 3 and 24 characters long and contain only lowercase letters and numbers."
   }
 }
